8dde7c29bda026b3c9735b9cf05197c685b72fdc
Anderson Kaiti <anderkaiti@gmail.com>
2026-08-20 22:51:38 -0300

    :sparkles: feat(scripts): seed customers and orders with a loop

--- scripts/3-insert.sql
+++ scripts/3-insert.sql
@@ -13,6 +13,27 @@
 --   ('Eduardo', 'Silva', 'eduardo@gmail.com')
 -- ;
 
-INSERT INTO customers (first_name, last_name, email)
-VALUES ('Daniel', 'Silva', 'daniel@gmail.com')
-RETURNING *;
+-- INSERT INTO customers (first_name, last_name, email)
+-- VALUES ('Daniel', 'Silva', 'daniel@gmail.com')
+-- RETURNING *;
+
+DO $$
+DECLARE
+  i INT := 1;
+  customer_id INT;
+  created_customers INT := 0;
+BEGIN
+  WHILE i <= 20 LOOP
+    INSERT INTO customers(first_name, last_name, email)
+    VALUES ('Customer-' || i, 'Doe-' || i, 'customer.' || i || '@email.com')
+    RETURNING id INTO customer_id;
+
+    INSERT INTO orders(amount, customer_id)
+    VALUES (RANDOM() * 1000, customer_id);
+
+    i := i + 1;
+    created_customers := created_customers + 1;
+  END LOOP;
+
+  RAISE NOTICE '% customers succesfully created', created_customers;
+END $$
--- scripts/4-select.sql
+++ scripts/4-select.sql
@@ -1,3 +1,4 @@
 \c sql_fundamentals
 
 SELECT * FROM customers;
+SELECT * FROM orders;
