5331e0969f4a60d74b67d3732dd9f9e5d153a4d1
Anderson Kaiti <anderkaiti@gmail.com>
2026-08-21 02:36:14 -0300

    :sparkles: feat(scripts): add select with order by, offset and limit

--- scripts/4-select.sql
+++ scripts/4-select.sql
@@ -1,4 +1,14 @@
 \c sql_fundamentals
 
-SELECT * FROM customers;
-SELECT * FROM orders;
+-- SELECT id, first_name AS "firstName", last_name AS "lastName"
+-- FROM customers;
+
+-- SELECT * FROM customers;
+-- SELECT * FROM orders;
+
+SELECT * FROM customers
+ORDER BY id ASC
+-- offset is an index, so it starts at 0
+OFFSET 10
+LIMIT 10
+;
