c14f658aa8f91646e6294fa44859dc72857eb836
Anderson Kaiti <anderkaiti@gmail.com>
2026-08-20 21:58:39 -0300

    :sparkles: feat(scripts): add orders table and email column to customers

--- scripts/1-create-table.sql
+++ scripts/1-create-table.sql
@@ -10,5 +10,17 @@ CREATE TABLE IF NOT EXISTS customers (
   -- varchar (VARIABLE CHARACTER)
   first_name VARCHAR(20),
   last_name VARCHAR(60),
+  email VARCHAR(256),
   created_at TIMESTAMP DEFAULT NOW()
+);
+
+DROP TABLE IF EXISTS orders;
+
+-- postgresql has a table to controls the id's sequency: tablename_colname_seq
+-- `colname SERIAL` is equivalent to `colname integer NOT NULL DEFAULT nextval('tablename_colname_seq')`
+
+CREATE TABLE IF NOT EXISTS orders (
+  id SERIAL,
+  customer_id INT,
+  amount NUMERIC(7, 2)
 );
--- scripts/2-alter-table.sql
+++ scripts/2-alter-table.sql
@@ -25,6 +25,6 @@
 -- ALTER TABLE customers
 -- DROP COLUMN age;
 
-ALTER TABLE customers
-ADD COLUMN
-age INT DEFAULT 0;
+-- ALTER TABLE customers
+-- ADD COLUMN
+-- age INT DEFAULT 0;
