79ef5cfb4249f5ec1088d31049dafd7e3b07e1d4
Anderson Kaiti <anderkaiti@gmail.com>
2026-08-21 03:05:37 -0300

    :sparkles: feat(scripts): add select with where clause filter examples

--- scripts/4-select.sql
+++ scripts/4-select.sql
@@ -6,9 +6,25 @@
 -- SELECT * FROM customers;
 -- SELECT * FROM orders;
 
+-- SELECT * FROM customers
+-- ORDER BY id ASC
+-- -- offset is an index, so it starts at 0
+-- OFFSET 10
+-- LIMIT 10
+-- ;
+
+-- SELECT * FROM customers
+-- WHERE (id >= 20 OR last_name = 'Doe-1') OR first_name = 'Customer-2'
+-- ;
+
 SELECT * FROM customers
-ORDER BY id ASC
--- offset is an index, so it starts at 0
-OFFSET 10
-LIMIT 10
+WHERE
+  -- id IN(1, 2, 3)
+  -- id BETWEEN 12 AND 16
+  -- id NOT BETWEEN 12 AND 16
+  -- id NOT IN(1, 2, 3)
+  -- email IS NULL
+  -- first_name LIKE '%2%'
+  first_name ILIKE '%customer%'
+  -- iLIKE means 'Insensitive LIKE'
 ;
