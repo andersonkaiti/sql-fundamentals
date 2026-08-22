3a44065b41253c6d6cc475925abeb54d509a6f8b
Anderson Kaiti <anderkaiti@gmail.com>
2026-08-22 16:58:02 -0300

    :sparkles: feat(count): add count examples for total rows and non-null emails

--- scripts/1-functions.sql
+++ scripts/1-functions.sql
@@ -1,5 +1,16 @@
 \c sql_fundamentals;
 
--- the keyword "AS" can be omitted
-SELECT id, CONCAT(first_name, ' ', last_name) "fullName"
+-- -- the keyword "AS" can be omitted
+-- SELECT id, CONCAT(first_name, ' ', last_name) "fullName"
+-- FROM customers;
+
+-- a row without email:
+INSERT INTO customers (first_name) VALUES ('Anderson');
+
+-- how many rows have in the table
+SELECT COUNT(*) total_of_customers
+FROM customers;
+
+-- how many rows have the email column
+SELECT COUNT(email) total_of_customers
 FROM customers;
