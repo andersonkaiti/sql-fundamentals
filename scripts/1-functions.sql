dbea39af7c8c610c59771e0f66640ed6c98d3945
Anderson Kaiti <anderkaiti@gmail.com>
2026-08-23 19:09:07 -0300

    :sparkles: feat(sum): add sum to calculate total revenue from orders

--- scripts/1-functions.sql
+++ scripts/1-functions.sql
@@ -5,16 +5,23 @@
 -- FROM customers;
 
 -- a row with an email:
-INSERT INTO customers (first_name, email) VALUES ('Anderson', 'customer.1@email.com');
+-- INSERT INTO customers (first_name, email) VALUES ('Anderson', 'customer.1@email.com');
 
 -- -- how many rows are in the table
 -- SELECT COUNT(*) total_of_customers
 -- FROM customers;
 
 -- how many rows have the email column
+-- SELECT
+--   COUNT(
+--     -- the DISTINCT keyword returns unique values from the column
+--     DISTINCT email
+--   ) total_of_customers
+-- FROM customers;
+
+-- INSERT INTO orders (customer_id, amount) VALUES (1, 1002);
+
 SELECT
-  COUNT(
-    -- the DISTINCT keyword returns unique values from the column
-    DISTINCT email
-  ) total_of_customers
-FROM customers;
+  SUM(amount) "totalRevenue",
+  COUNT(id) "totalOrders"
+FROM orders;
