66ec37b778bb30e50ee898592d1788e2784d524c
Anderson Kaiti <anderkaiti@gmail.com>
2026-08-24 22:59:47 -0300

    :sparkles: feat(server): add pg client to query customers_summary view

---
+++ server/main.ts
@@ -0,0 +1,14 @@
+import { Client } from 'pg'
+
+const client = new Client({
+  connectionString: 'postgres://root:root@localhost:5432/sql_fundamentals',
+})
+
+await client.connect()
+
+const res = await client.query(`
+  SELECT * FROM customers_summary
+  ORDER BY total_revenue DESC
+`)
+
+console.log(res.rows)
