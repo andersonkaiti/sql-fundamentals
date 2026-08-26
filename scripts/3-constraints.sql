5aa2391af9a5f99fcc9ae8a80de6f311e6738d5c
Anderson Kaiti <anderkaiti@gmail.com>
2026-08-25 22:17:03 -0300

    :sparkles: feat(constraints): add and inspect UNIQUE constraint on customers email

--- scripts/3-constraints.sql
+++ scripts/3-constraints.sql
@@ -1,16 +1,30 @@
 \c sql_fundamentals
 
-INSERT INTO customers (first_name)
-VALUES ('Zezinho');
+-- INSERT INTO customers (first_name)
+-- VALUES ('Zezinho');
 
-SELECT * FROM customers;
+-- SELECT * FROM customers;
 
 -- UPDATE customers
 -- SET email = 'zezinho@gmail.com'
 -- WHERE id = 21;
 
--- -- constraints -> limitations
+-- -- -- constraints -> limitations
+-- ALTER TABLE customers
+-- ALTER COLUMN email
+-- -- whenever i register a new customer, their email must not be null
+-- SET NOT NULL;
+
 ALTER TABLE customers
-ALTER COLUMN email
--- whenever i register a new customer, their email must not be null
-SET NOT NULL;
+-- this constraint grants that the email never will be null neither duplicated
+ADD UNIQUE(email);
+
+INSERT INTO customers (first_name, email)
+VALUES ('Zé', null);
+
+ALTER TABLE customers
+DROP CONSTRAINT customers_email_key;
+
+SELECT constraint_name, constraint_type, table_name
+FROM information_schema.table_constraints
+WHERE table_schema = 'public';
